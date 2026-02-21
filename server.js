0b3b9c86a7d07dd65241a4cfb1700e1a95f75080
NKPPAO <itnkppao@gmail.com>
2026-02-21 19:12:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
-db.connect(err => {
-  if (err) console.log(err);
-  else console.log("MySQL Connected");
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
